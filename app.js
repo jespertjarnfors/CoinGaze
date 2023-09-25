ab9d2d2592ef503f1286682dc909da4325bcf19c
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2023-09-26 08:25:08 +1300

    Website functionality is complete. Removed app.js since it was obsolete.

--- app.js
+++
@@ -1,5 +0,0 @@
-
-
-fetch ('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&include_market_cap=true&include_24hr_change=tru&order=market_cap_desc&per_page=50')
-    .then(response => response.json())
-    .then((json) => console.log(json));
